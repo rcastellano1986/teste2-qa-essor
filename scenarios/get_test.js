58c75e619f62432fc86a815ada3fba7ea6a405ba
Raphael Costa <raphaelcastellano100@gmail.com>
2025-07-16 17:09:45 -0300

    Geração de arquivo em HTML do K6

--- scenarios/get_test.js
+++ scenarios/get_test.js
@@ -1,11 +1,18 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"; 
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data), // Gera o relatório HTML
+  };
+}
 
 export const options = {
   vus: 5,         // Usuários virtuais
   duration: '10s', // Duração do teste
   thresholds: {
-    http_req_duration: ['p(95) <100'], //95% das requisicoes devem responder em até 100ms
+    http_req_duration: ['p(95) < 2000'], //95% das requisicoes devem responder em até 2seg
     http_req_failed: ['rate<0.01'] //1% das requisicoes podem ocorrer erro
   }
 };
