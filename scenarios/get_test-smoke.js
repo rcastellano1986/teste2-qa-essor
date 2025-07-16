3348234080b53b54c97bf5dc98e88eb22dad4f71
Raphael Costa <raphaelcastellano100@gmail.com>
2025-07-16 16:39:18 -0300

    Teste de fumaça

---
+++ scenarios/get_test-smoke.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 1,         // Usuários virtuais
+  duration: '1m', // Duração do teste
+  thresholds: {
+    http_req_duration: ['p(95) < 2000'], //95% das requisicoes devem responder em até 2seg
+    http_req_failed: ['rate<0.01'] //1% das requisicoes podem ocorrer erro
+  }
+};
+
+export default function () {
+  const res = http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  check(res, {
+    'GET Status is 200': (r) => r.status === 200,
+    'GET Body contains title': (r) => r.json().hasOwnProperty('title'),
+    'GET Response time is < 5000ms': (r) => r.timings.duration < 5000,
+  });
+
+  sleep(1); // Pausa de 1 segundo entre as iterações
+}
